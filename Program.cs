eb926af2ec75f842df97d21cd8be1dd77720cc81
Ryan <ryanprogramacaoedev@gmail.com>
2025-09-05 20:50:04 -0300

    eu criei uma contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
